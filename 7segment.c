4b6212a72ef1629104b1e1039181557b868653a2
Heinzino <51274202+Heinzino@users.noreply.github.com>
2024-03-29 16:52:24 -0600

    Update 7segment.c
    Function desc. and cleaner code

--- 7segment.c
+++ 7segment.c
@@ -1,8 +1,8 @@
 /*
- * File:   7segment.c
+ * File:   main.c
  * Author: heinz
  *
- * Created on February 20, 2024, 12:11 PM
+ * Created on March 11, 2024, 11:12 AM
  */
 
 
@@ -11,27 +11,20 @@
 #include <util/delay.h>
 #include <stdint.h>
 
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
 
-//DS_pin PA1 (data pin,blue)
-//STCP   PA2 (serial clock, white)
-//STHP   PA3 (latch/register clock,red)
+u8 getFirstDigit(int temp);
+void displayDigit(int numToDisplay);
 
 //DS_pin PA4 (data pin,blue,MOSI)
 //STCP   PA6 (serial clock, white, SCK)
 //STHP   PA7 (latch clock,red, SS)
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-
-void serialPulse(void);
-void displayDigit(int numToDisplay);
-void displayTemperature(int temp);
-u8 getData(int temp);
-
-
+//Common Cathode configurations for 7seg LEDs A-G
 u8 au8_digits[11] = {
-  0b011111110, //0
+  0b01111110, //0
   0b00110000, //1
   0b01101101, //2
   0b01111001, //3
@@ -44,87 +37,80 @@ u8 au8_digits[11] = {
   0b00000001, //Negative sign
 };
 
-
-u8 test_digits[2] = {
-  0b000000000, //0
-  0b000000001
-};
-
-
-
-// |= HIGH
-// &= LOW 
-
-
 int main(void) {
     /* Replace with your application code */
     
     PORTA.DIRSET = 0b11010000; //Set ports to output
-    //PORTA.DIRSET = 0b00001110; NON SPI OUTPUT Ports
-    //SPI config for HC595 chip
-    SPI0.CTRLA = 0b01110011; //Enable SPI, LSB, 4MHz SPI frequency
-    SPI0.CTRLB = 0b00000100; //Disable SS to manually use as a latch
+    PORTA.OUT &= 0b00101111;
     
- 
-   while (1) {
+    SPI0.CTRLA = 0b01100011; //Enable SPI, LSB, 12 MHZ SPI Frequency
+    SPI0.CTRLB = 0b00000100; //Disable SS to manually use as a latch
+
+    while (1) {
+        displayDigit(2);
+        _delay_ms(100);
+       /*
         for(int i =0; i < 10; ++i){
-            displayTemperature(i);
-            _delay_ms(100);
+            displayDigit(i);
+            _delay_ms(300);
         }
+       */
+      
     }
 }
 
-void serialPulse(void){
-    PORTA.OUT &= 0b11111011; //Serial Clock LOW
-    _delay_loop_1(1);
-    PORTA.OUT |= 0b00000100; //Serial Clock HIGH
-    _delay_loop_1(1);
-}
+/*
+@fn  void displayTemperature(int temp)
+@brief Displays two digit temperature onto the seven segment
 
+REQUIRES:
+-2 digit temp in degree celsius (-9 to 99)
+ 
+PROMISES:
+- Displays two digit temperature onto the seven segment
+*/
 void displayDigit(int numToDisplay){
     
-    u8 data = getData(numToDisplay); 
+    u8 secondDigit = au8_digits[(numToDisplay%10)];
+    //Send
     
-    PORTA.OUT &= 0b11110111; //Latch LOW
-
-    //Send data serially bigEndian style, update for 16 bit later
-    for(int i = 0; i < 8; i++){
-        if(data & 0b1000000){
-            PORTA.OUT |= 0b00000010; //Data PIN High
-        }
-        else{
-            PORTA.OUT &= 0b11111101; //Data PIN Low
-        }
-        serialPulse();
-        data = data<<1 ;
-    }
+    u8 firstDigit = getFirstDigit(numToDisplay);
+    //Send testing just one digit at the bottom
     
-    PORTA.OUT |= 0b00001000; //Latch HIGH to update the register
-}
-
-void displayTemperature(int temp){
-    u8 data = getData(temp); //Change to 16 bit handle later
-    
+    u8 data = au8_digits[numToDisplay];
+    char flush_buffer;
     PORTA.OUT &= 0b01111111; //Latch Low
     SPI0.DATA = data; 
     while(!(SPI0.INTFLAGS & 0b10000000)); //Wait for SPI data to transfer.
-    SPI0.INTFLAGS &= 0b01111111; //Erase interrupt flag
-    PORTA.OUT |= 0b1000000; //Latch High
+    flush_buffer = SPI0.DATA; //IF Flag cleared by reading IF then accessing Data
+    PORTA.OUT |= 0b10000000; //Latch High
 
+    //Need to make pulse larger HIGH maybe us delay
+    //Trying swithing high then low did not work still need a pulse
 }
 
-u8 getData(int temp){ 
-    u8 firstDigit, secondDigit;
-    
-    secondDigit = au8_digits[(temp % 10)];
-    
+
+/*
+@fn  void getFirstDigit(int temp)
+@brief Breaks down first digit of temp to either get negative sign or numeric digit
+
+REQUIRES:
+-2 digit temp in degree celsius (-9 to 99)
+-au8_digits holds proper data
+ 
+PROMISES:
+- Returns proper data to output based on the first digit of temperature; 
+ handling negative signs
+*/
+u8 getFirstDigit(int temp){
+    u8 firstDigit;
     if(temp < 0){
         firstDigit = au8_digits[10]; //Negative sign
     }
     else{
         firstDigit = au8_digits[(temp/10)%10];
     }
-    
-    u16 data = (firstDigit << 8 ) + secondDigit;
-    return au8_digits[temp];
+    return firstDigit;
 }
+
+
